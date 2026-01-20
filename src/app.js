379cdc2b03fdff452cbad0eac96912c67ee20d1d
SanathRai <sanathrai03@gmail.com>
2026-01-20 23:25:07 +0530

    Update/PackaggeController (Update package controller for getAllPackages)

--- src/app.js
+++ src/app.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-// const authRoutes = require('../src/routes/auth.route.js')
-// const foodRoutes = require('../src/routes/food.route.js')
-// const partnerRoutes = require('../src/routes/partner.route.js')
-// const userRoutes = require('../src/routes/user.route.js')
-// const orderRoutes = require('../src/routes/order.route.js')
 const packageRouter = require('../src/routes/package.route.js')
 
 const app = express();
-// app.use(cors({
-//     origin: process.env.FRONTEND_URL,
-//     credentials: true
-// }));
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    // credentials: true
+}));
 app.use(cookieParser())
 app.use(express.json());
 
@@ -21,10 +16,5 @@ app.get("/", (req, res)=>{
 })
 
 app.use('/api/packages', packageRouter);
-// app.use('/api/auth', authRoutes);
-// app.use('/api/food', foodRoutes);
-// app.use('/api/partner', partnerRoutes);
-// app.use('/api/user', userRoutes);
-// app.use('/api/order', orderRoutes)
 
 module.exports = app;
--- src/controllers/package.controller.js
+++ src/controllers/package.controller.js
@@ -2,24 +2,24 @@ const PackageModel = require('../models/package.model.js');
 
 const getAllPackages = async (req, res) => {
     try {
-        const packages = await PackageModel.find();
+        const packages = await PackageModel.find()
+            .select(
+                "title images duration location basePrice categories"
+            );
 
         return res.status(200).json({
             success: true,
             count: packages.length,
             data: packages
         });
-
-        console.log(packages)
     } catch (error) {
-        console.error("Error fetching package data:", error);
-
         return res.status(500).json({
             success: false,
-            message: "Failed to fetch package data",
+            message: "Failed to fetch packages",
             error: error.message
         });
     }
+
 };
 
 module.exports = { getAllPackages }
